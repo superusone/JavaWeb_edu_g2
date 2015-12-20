dc56d9af75c73b1561f76df0819d7e169ae4d2dd
hanhao <ihanhao@foxmail.com>
2015-12-20 21:32:16 +0800

    试题JavaBean更新以下字段 	id、试题类型（0,1,2 分别代表单选，多选，判断）、题目、答案、选项、备注

--- JavaWeb_edu_g2/src/cn/zjxu/exam/paper/domain/Paper.java
+++ JavaWeb_edu_g2/src/cn/zjxu/exam/paper/domain/Paper.java
@@ -1,16 +1,17 @@
 package cn.zjxu.exam.paper.domain;
 
 public class Paper {
-	private String pid; //试题id
-	private int type;//试题类型  0：单选 1：多选 2：判断
-	private String q_subject; //题目
-	private String q_answer;//答案
-	//选项
+	private String pid; // 试题id
+	private int type;// 试题类型 0：单选 1：多选 2：判断
+	private String q_subject; // 题目
+	private String q_answer;// 答案
+	// 选项
 	private String optionA;
 	private String optionB;
 	private String optionC;
 	private String optionD;
-	private String note; //备注
+	private String note; // 备注
+
 	public Paper(String pid, int type, String q_subject, String q_answer,
 			String optionA, String optionB, String optionC, String optionD,
 			String note) {
@@ -25,63 +26,83 @@ public class Paper {
 		this.optionD = optionD;
 		this.note = note;
 	}
+
 	public Paper() {
 		super();
 	}
+
 	public String getPid() {
 		return pid;
 	}
+
 	public void setPid(String pid) {
 		this.pid = pid;
 	}
+
 	public int getType() {
 		return type;
 	}
+
 	public void setType(int type) {
 		this.type = type;
 	}
+
 	public String getQ_subject() {
 		return q_subject;
 	}
+
 	public void setQ_subject(String q_subject) {
 		this.q_subject = q_subject;
 	}
+
 	public String getQ_answer() {
 		return q_answer;
 	}
+
 	public void setQ_answer(String q_answer) {
 		this.q_answer = q_answer;
 	}
+
 	public String getOptionA() {
 		return optionA;
 	}
+
 	public void setOptionA(String optionA) {
 		this.optionA = optionA;
 	}
+
 	public String getOptionB() {
 		return optionB;
 	}
+
 	public void setOptionB(String optionB) {
 		this.optionB = optionB;
 	}
+
 	public String getOptionC() {
 		return optionC;
 	}
+
 	public void setOptionC(String optionC) {
 		this.optionC = optionC;
 	}
+
 	public String getOptionD() {
 		return optionD;
 	}
+
 	public void setOptionD(String optionD) {
 		this.optionD = optionD;
 	}
+
 	public String getNote() {
 		return note;
 	}
+
 	public void setNote(String note) {
 		this.note = note;
 	}
+
 	@Override
 	public String toString() {
 		return "Paper [pid=" + pid + ", type=" + type + ", q_subject="
@@ -89,6 +110,5 @@ public class Paper {
 				+ ", optionB=" + optionB + ", optionC=" + optionC
 				+ ", optionD=" + optionD + ", note=" + note + "]";
 	}
-	
-	
+
 }
