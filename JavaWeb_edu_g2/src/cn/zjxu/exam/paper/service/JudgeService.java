3925692b8ba9f7a9f0d76c8226b001f5beaa7398
lvzian <Administrator@VXU3JVVLRUYNUZ9>
2016-01-01 15:45:10 +0800

    创建一个JudgeService类判断答案是否正确

---
+++ JavaWeb_edu_g2/src/cn/zjxu/exam/paper/service/JudgeService.java
@@ -0,0 +1,49 @@
+package cn.zjxu.exam.paper.service;
+
+import cn.zjxu.exam.paper.dao.PaperDao;
+import cn.zjxu.exam.paper.web.servlet.PaperServlet;
+import cn.zjxu.exam.sp.dao.SPDao;
+import cn.zjxu.exam.sp.domain.SP;
+
+/**
+ * 判断答案是否正确
+ * 
+ * @author lunatic
+ *
+ */
+public class JudgeService extends PaperServlet {
+	private SPDao spDao = new SPDao();
+	private PaperDao paperDao = new PaperDao();
+
+	/**
+	 * 判断答案是否正确
+	 * 
+	 * @param id
+	 *            用户id
+	 * @param pid
+	 *            题目编号
+	 * @param userAnswer
+	 *            用户答案
+	 */
+	public void Judge(String id, String pid, String userAnswer) {
+
+		String answer = paperDao.getAnswer(pid);
+		int grade;
+		SP sp = new SP();
+		if (userAnswer.equals(answer)) {
+			grade = 10;
+			sp.setGrade(grade);
+			sp.setId(id);
+			sp.setPid(pid);
+			spDao.add(sp);
+
+		} else {
+			grade = 0;
+			sp.setGrade(grade);
+			sp.setId(id);
+			sp.setPid(pid);
+			spDao.add(sp);
+		}
+	}
+
+}
