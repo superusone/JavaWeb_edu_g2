3eb002a491c3e0d1eb6900d8bfb559e5577e3da9
hukaijun <857929558@qq.com>
2016-01-02 14:57:56 +0800

    向Servlet返回查询的试卷类型

--- JavaWeb_edu_g2/src/cn/zjxu/exam/paper/web/servlet/ShowPaperServlet.java
+++ JavaWeb_edu_g2/src/cn/zjxu/exam/paper/web/servlet/ShowPaperServlet.java
@@ -1,13 +1,19 @@
 package cn.zjxu.exam.paper.web.servlet;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import cn.zjxu.exam.paper.service.ShowPaperService;
+
 
 public class ShowPaperServlet extends PaperServlet {
+	private ShowPaperService sps = new ShowPaperService();
+	
 	public void ShowPaper(HttpServletRequest request,
 			HttpServletResponse response){
 		String papertype = request.getParameter("papertype");
-		
+		List paperTypedList = sps.findByNote(papertype);
 	}
 }
