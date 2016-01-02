c22cf3e4ce5f43bbf6cabe5f0f1b396e21993737
hukaijun <857929558@qq.com>
2016-01-02 15:23:05 +0800

    将试题返回给页面

--- JavaWeb_edu_g2/src/cn/zjxu/exam/paper/web/servlet/ShowPaperServlet.java
+++ JavaWeb_edu_g2/src/cn/zjxu/exam/paper/web/servlet/ShowPaperServlet.java
@@ -1,19 +1,40 @@
 package cn.zjxu.exam.paper.web.servlet;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import cn.zjxu.exam.paper.domain.Paper;
 import cn.zjxu.exam.paper.service.ShowPaperService;
-
 
 public class ShowPaperServlet extends PaperServlet {
 	private ShowPaperService sps = new ShowPaperService();
-	
-	public void ShowPaper(HttpServletRequest request,
-			HttpServletResponse response){
+
+	public String ShowPaper(HttpServletRequest request,
+			HttpServletResponse response) {
 		String papertype = request.getParameter("papertype");
-		List paperTypedList = sps.findByNote(papertype);
+		List<Paper> paperTypedList = sps.findByNote(papertype); 
+		List<Paper> singleList = new ArrayList<Paper>();
+		List<Paper> multiselectList = new ArrayList<Paper>();
+		List<Paper> judgeList = new ArrayList<Paper>();
+		for (Paper p : paperTypedList) {
+			if(p.getType()==1){
+				//单选
+				singleList.add(p);
+			}else if(p.getType()==2){
+				//多选
+				multiselectList.add(p);
+			}else{
+				//判断
+				judgeList.add(p);
+			}
+		}
+		request.setAttribute("singleList", singleList);
+		request.setAttribute("multiselectList", multiselectList);
+		request.setAttribute("judgeList",judgeList);
+		
+		return "f:/jsps/paper/desc.jsp";
 	}
 }
